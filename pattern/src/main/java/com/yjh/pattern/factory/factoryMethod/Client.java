ef40f3027587ea226aa766faa4a0d2cb5002fffe
yjh <461941298@qq.com>
2018-04-25 11:58:40 +0800

    添加一些东西

---
+++ pattern/src/main/java/com/yjh/pattern/factory/factoryMethod/Client.java
@@ -0,0 +1,12 @@
+package com.yjh.pattern.factory.factoryMethod;
+
+/**
+ * Copyright © 2017/12/18 Shanxi WenDao Culture and Technology Co., Ltd. All rights reserved.
+ * <p>
+ * Author: yjh
+ * Date: 2018/4/25 11:56
+ * Version: 1.0
+ * Desc: 工厂方法
+ */
+public class Client {
+}
---
+++ pattern/src/main/java/com/yjh/pattern/factory/simpleFacotry/Client.java
@@ -0,0 +1,19 @@
+package com.yjh.pattern.factory.simpleFacotry;
+
+import org.junit.Test;
+
+/**
+ * Copyright © 2017/12/18 Shanxi WenDao Culture and Technology Co., Ltd. All rights reserved.
+ * <p>
+ * Author: yjh
+ * Date: 2018/4/25 11:52
+ * Version: 1.0
+ * Desc: 简单工厂
+ */
+public class Client {
+
+    @Test
+    public void test() {
+        System.out.println("test");
+    }
+}
