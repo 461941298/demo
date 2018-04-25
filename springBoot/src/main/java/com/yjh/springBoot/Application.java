1470a7fcce2e7b6f836d5f0a395f8872fc50dbd6
yjh <461941298@qq.com>
2018-04-25 14:48:27 +0800

    添加springboot

---
+++ springBoot/src/main/java/com/yjh/springBoot/Application.java
@@ -0,0 +1,20 @@
+package com.yjh.springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright © 2017/12/18 Shanxi WenDao Culture and Technology Co., Ltd. All rights reserved.
+ * <p>
+ * Author: yjh
+ * Date: 2018/4/25 14:35
+ * Version: 1.0
+ * Desc:
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
