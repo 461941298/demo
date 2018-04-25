cf114c76d1b0fc42b690586d4fc4e2722495841f
yjh <461941298@qq.com>
2018-04-25 10:14:10 +0800

    ?

---
+++ src/main/java/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright © 2017/12/18 Shanxi WenDao Culture and Technology Co., Ltd. All rights reserved.
+ * <p>
+ * Author: yjh
+ * Date: 2018/4/25 9:30
+ * Version: 1.0
+ * Desc:
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello demo!");
+    }
+}
