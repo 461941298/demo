0ff1a833591d15a6d6b528ad91cbd6bc85e06cdc
yjh <461941298@qq.com>
2018-04-28 11:35:37 +0800

    一些改动

--- pattern/src/main/java/com/yjh/pattern/constructionType/adaptor/Client.java
+++ pattern/src/main/java/com/yjh/pattern/constructionType/adaptor/Client.java
@@ -1,4 +1,12 @@
 package com.yjh.pattern.constructionType.adaptor;
 
+/**
+ * Copyright © 2017/12/18 Shanxi WenDao Culture and Technology Co., Ltd. All rights reserved.
+ * <p>
+ * Author: yjh
+ * Date: 2018/4/28 11:34
+ * Version: 1.0
+ * Desc: 适配器模式
+ */
 public class Client {
 }
