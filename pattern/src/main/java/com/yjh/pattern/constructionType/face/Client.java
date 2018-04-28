7602b7e79e4290cc0a4281985eebc584c749c25f
yjh <461941298@qq.com>
2018-04-28 11:54:17 +0800

    外观模式

--- pattern/src/main/java/com/yjh/pattern/constructionType/face/Client.java
+++ pattern/src/main/java/com/yjh/pattern/constructionType/face/Client.java
@@ -9,4 +9,9 @@ package com.yjh.pattern.constructionType.face;
  * Desc: 外观模式
  */
 public class Client {
+
+    /**
+     * 外观模式, 与其说是一个设计模式,不如说是一个架构模式
+     * 它的主要思想是将提供一个简单的接口(或者说是外部系统)从而将复杂的内部逻辑(或者架构)隐蔽起来
+     */
 }
