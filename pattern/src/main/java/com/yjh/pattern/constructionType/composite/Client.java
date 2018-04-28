918905c26554d0a4bfb2b0b240e97f90a433442c
yjh <461941298@qq.com>
2018-04-28 11:49:34 +0800

    组合模式

--- pattern/src/main/java/com/yjh/pattern/constructionType/composite/Client.java
+++ pattern/src/main/java/com/yjh/pattern/constructionType/composite/Client.java
@@ -9,4 +9,9 @@ package com.yjh.pattern.constructionType.composite;
  * Desc: 组合模式
  */
 public class Client {
+
+    /**
+     * 其实就是一棵树
+     * 通过"树"这样的数据结构,表示"整体" 与 "部分" 之间的关系, 并使 整体 与 部分 拥有一致的访问性
+     */
 }
